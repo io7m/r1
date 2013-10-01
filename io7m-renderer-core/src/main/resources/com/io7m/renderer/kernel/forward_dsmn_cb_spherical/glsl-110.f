5418155298b7c5ec4f3743fb0b93c93dd20b1366
m0 <m0>
2013-10-01 20:29:19 +0000

    Add Normals.bump and use it

--- io7m-renderer-core/src/main/resources/com/io7m/renderer/kernel/forward_dsmn_cb_spherical/glsl-110.f
+++ io7m-renderer-core/src/main/resources/com/io7m/renderer/kernel/forward_dsmn_cb_spherical/glsl-110.f
@@ -54,6 +54,28 @@ p_com_io7m_renderer_Normals_unpack (sampler2D map, vec2 uv)
 }
 
 vec3
+p_com_io7m_renderer_Normals_transform (vec3 m, vec3 t, vec3 b, vec3 n)
+{
+  mat3 mat = mat3(t, b, n);
+  {
+    return normalize((mat * m));
+  }
+}
+
+vec3
+p_com_io7m_renderer_Normals_bump (sampler2D t_normal, mat3 m_normal, vec3 n, vec3 t, vec3 b, vec2 uv)
+{
+  vec3 m = p_com_io7m_renderer_Normals_unpack(t_normal, uv);
+  vec3 nn = normalize(n);
+  vec3 nt = normalize(t);
+  vec3 nb = normalize(b);
+  vec3 r = p_com_io7m_renderer_Normals_transform(m, nt, nb, nn);
+  {
+    return (m_normal * r);
+  }
+}
+
+vec3
 p_com_io7m_renderer_SphericalLight_specular_color (pt_com_io7m_renderer_SphericalLight_t light, pt_com_io7m_renderer_SphericalLight_directions d, pt_com_io7m_renderer_Materials_t material)
 {
   float factor = pow(max(0.0, dot(d.reflection, d.stl)), material.specular_exponent);
@@ -100,15 +122,6 @@ p_com_io7m_renderer_SphericalLight_diffuse_specular (pt_com_io7m_renderer_Spheri
   }
 }
 
-vec3
-p_com_io7m_renderer_Normals_transform (vec3 m, vec3 t, vec3 b, vec3 n)
-{
-  mat3 mat = mat3(t, b, n);
-  {
-    return normalize((mat * m));
-  }
-}
-
 pt_com_io7m_renderer_Materials_t
 _tmp_8 (void)
 {
@@ -121,14 +134,9 @@ _tmp_8 (void)
 void
 main (void)
 {
-  vec3 m = p_com_io7m_renderer_Normals_unpack(t_normal, f_uv);
-  vec3 n = normalize(f_normal);
-  vec3 t = normalize(f_tangent);
-  vec3 b = normalize(f_bitangent);
-  vec3 r = p_com_io7m_renderer_Normals_transform(m, t, b, n);
-  vec3 e = (m_normal * r);
+  vec3 n = p_com_io7m_renderer_Normals_bump(t_normal, m_normal, normalize(f_normal), normalize(f_tangent), normalize(f_bitangent), f_uv);
   pt_com_io7m_renderer_Materials_t mp = _tmp_8();
-  vec3 light_term = p_com_io7m_renderer_SphericalLight_diffuse_specular(light, e, f_position.xyz, mp);
+  vec3 light_term = p_com_io7m_renderer_SphericalLight_diffuse_specular(light, n, f_position.xyz, mp);
   vec3 albedo = texture2D(t_diffuse_0, f_uv).xyz;
   vec4 rgba = vec4((albedo * light_term), 1.0);
   gl_FragColor = rgba;
