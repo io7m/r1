6ca95cf64daebf152869d6d62c0f03c028061238
Mark Raynsford <code@io7m.com>
2014-04-05 14:08:03 +0000

    Fix debug depth renderer. Affects [fba03db9ea].

--- io7m-renderer-kernel/src/main/java/com/io7m/renderer/kernel/KRendererDebugDepth.java
+++ io7m-renderer-kernel/src/main/java/com/io7m/renderer/kernel/KRendererDebugDepth.java
@@ -362,10 +362,18 @@ final class KRendererDebugDepth extends KAbstractRendererDebug
     try {
       gc.viewportSet(framebuffer.kFramebufferGetArea());
 
+      gc.blendingDisable();
+
+      gc.colorBufferMask(true, true, true, true);
+      gc.colorBufferClearV4f(this.background);
+
+      gc.cullingDisable();
+
+      gc.depthBufferWriteEnable();
       gc.depthBufferTestEnable(DepthFunction.DEPTH_LESS_THAN);
       gc.depthBufferClear(1.0f);
-      gc.colorBufferClearV4f(KRendererDebugDepth.this.background);
-      gc.blendingDisable();
+
+      gc.viewportSet(framebuffer.kFramebufferGetArea());
 
       final Set<KInstanceTransformed> instances = scene.getVisibleInstances();
 
