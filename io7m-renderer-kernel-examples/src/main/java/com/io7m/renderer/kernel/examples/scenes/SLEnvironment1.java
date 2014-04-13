69ef5dba08df45f038d65d47b522fe09082d2fbb
Mark Raynsford <code@io7m.com>
2014-04-13 12:12:26 +0000

    Add environment mapping examples

---
+++ io7m-renderer-kernel-examples/src/main/java/com/io7m/renderer/kernel/examples/scenes/SLEnvironment1.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright © 2014 <code@io7m.com> http://io7m.com
+ * 
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+ * IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+package com.io7m.renderer.kernel.examples.scenes;
+
+import java.util.List;
+
+import javax.annotation.Nonnull;
+
+import com.io7m.jaux.Constraints.ConstraintError;
+import com.io7m.renderer.kernel.examples.ExampleSceneBuilderType;
+import com.io7m.renderer.kernel.examples.ExampleSceneType;
+import com.io7m.renderer.kernel.examples.ExampleSceneUtilities;
+import com.io7m.renderer.kernel.examples.ExampleViewType;
+import com.io7m.renderer.kernel.types.KFaceSelection;
+import com.io7m.renderer.kernel.types.KInstanceOpaqueRegular;
+import com.io7m.renderer.kernel.types.KInstanceTransformedOpaqueRegular;
+import com.io7m.renderer.kernel.types.KMaterialEnvironment;
+import com.io7m.renderer.kernel.types.KMaterialOpaqueRegular;
+import com.io7m.renderer.kernel.types.KMaterialSpecular;
+import com.io7m.renderer.types.RException;
+
+/**
+ * A demonstration that environment mapping (with specularity) looks correct.
+ */
+
+public final class SLEnvironment1 implements ExampleSceneType
+{
+  /**
+   * Construct the example.
+   */
+
+  public SLEnvironment1()
+  {
+
+  }
+
+  @Override public String exampleGetName()
+  {
+    return this.getClass().getCanonicalName();
+  }
+
+  @Override public void exampleScene(
+    final @Nonnull ExampleSceneBuilderType scene)
+    throws ConstraintError,
+      RException
+  {
+    final KMaterialOpaqueRegular material =
+      ExampleSceneUtilities.OPAQUE_MATTE_WHITE.withEnvironment(
+        KMaterialEnvironment.newEnvironmentMapped(
+          1.0f,
+          scene.cubeTexture("toronto/cube.rxc"),
+          false)).withSpecular(
+        KMaterialSpecular.newSpecularUnmapped(
+          ExampleSceneUtilities.RGB_WHITE,
+          64.0f));
+
+    final KInstanceTransformedOpaqueRegular i =
+      KInstanceTransformedOpaqueRegular.newInstance(
+        KInstanceOpaqueRegular.newInstance(
+          material,
+          scene.mesh("plane2x2_PN.rmx"),
+          KFaceSelection.FACE_RENDER_FRONT),
+        ExampleSceneUtilities.IDENTITY_TRANSFORM,
+        ExampleSceneUtilities.IDENTITY_UV);
+
+    scene.sceneAddOpaqueLitVisibleWithShadow(
+      ExampleSceneUtilities.LIGHT_SPHERICAL_LARGE_WHITE
+        .withPosition(ExampleSceneUtilities.CENTER),
+      i);
+  }
+
+  @Override public List<ExampleViewType> exampleViewpoints()
+  {
+    return ExampleSceneUtilities.STANDARD_VIEWS_CLOSE_3;
+  }
+}
