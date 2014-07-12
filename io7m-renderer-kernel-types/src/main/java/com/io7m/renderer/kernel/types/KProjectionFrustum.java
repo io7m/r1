8ef1d942babd43e9a092e81fd915356c96668ec9
Mark Raynsford <code@io7m.com>
2014-07-12 18:38:18 +0000

    Add frustum mesh types and caches.

--- io7m-renderer-kernel-types/src/main/java/com/io7m/renderer/kernel/types/KProjectionFrustum.java
+++ io7m-renderer-kernel-types/src/main/java/com/io7m/renderer/kernel/types/KProjectionFrustum.java
@@ -1,10 +1,10 @@
 /*
  * Copyright © 2014 <code@io7m.com> http://io7m.com
- *
+ * 
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
- *
+ * 
  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
  * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
@@ -50,7 +50,7 @@ public final class KProjectionFrustum implements KProjectionType
    * @return A new projection.
    */
 
-  public static KProjectionType newProjection(
+  public static KProjectionFrustum newProjection(
     final MatrixM4x4F temporary,
     final float in_x_min,
     final float in_x_max,
