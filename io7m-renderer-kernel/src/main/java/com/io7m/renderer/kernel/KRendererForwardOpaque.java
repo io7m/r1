19d4e8d648b5de2398f4386a5285b3df76d899d4
m0 <m0>
2014-07-22 21:34:36 +0000

    Fix unlit opaque rendering in the forward renderer (missing projection matrix re-use) and add visual test case.

--- io7m-renderer-kernel/src/main/java/com/io7m/renderer/kernel/KRendererForwardOpaque.java
+++ io7m-renderer-kernel/src/main/java/com/io7m/renderer/kernel/KRendererForwardOpaque.java
@@ -1,10 +1,10 @@
 /*
  * Copyright © 2014 <code@io7m.com> http://io7m.com
- *
+ * 
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
- *
+ * 
  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
  * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
@@ -135,6 +135,7 @@ import com.io7m.renderer.types.RExceptionJCGL;
           gc,
           instance.instanceGetFaceSelection());
 
+        KShadingProgramCommon.putMatrixProjectionReuse(program);
         KRendererCommon.putInstanceMatricesRegular(program, mwi, material);
         KRendererCommon
           .putInstanceTexturesRegular(context, program, material);
