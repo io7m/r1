f5198c1b755bac9e887ca5fedac3becaa8d25539
Mark Raynsford <code@io7m.com>
2014-07-31 19:46:24 +0000

    Add towers example

--- io7m-renderer-kernel/src/main/java/com/io7m/renderer/kernel/KRendererForwardOpaque.java
+++ io7m-renderer-kernel/src/main/java/com/io7m/renderer/kernel/KRendererForwardOpaque.java
@@ -1,10 +1,10 @@
 /*
  * Copyright © 2014 <code@io7m.com> http://io7m.com
- *
+ * 
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
- *
+ * 
  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
  * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
@@ -244,7 +244,9 @@ import com.io7m.renderer.types.RExceptionJCGL;
     throws RException,
       JCGLException
   {
-    KRendererCommon.putShadow(shadow_context, unit_context, program, light);
+    if (light.lightHasShadow()) {
+      KRendererCommon.putShadow(shadow_context, unit_context, program, light);
+    }
 
     KShadingProgramCommon.putLightProjectiveWithoutTextureProjection(
       program,
