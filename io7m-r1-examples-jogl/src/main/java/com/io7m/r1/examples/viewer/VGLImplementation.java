5279a0166119db368f4a8c3a449882924f3d7b9e
m0 <m0>
2014-10-11 19:27:11 +0000

    Abstract JCGLImplementation creation to make it easy to enable disable various features at a later date

---
+++ io7m-r1-examples-jogl/src/main/java/com/io7m/r1/examples/viewer/VGLImplementation.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright © 2014 <code@io7m.com> http://io7m.com
+ * 
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+ * IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+package com.io7m.r1.examples.viewer;
+
+import javax.media.opengl.GLContext;
+
+import com.io7m.jcanephora.api.JCGLImplementationType;
+import com.io7m.jcanephora.jogl.JCGLImplementationJOGL;
+import com.io7m.jcanephora.jogl.JCGLImplementationJOGLBuilderType;
+import com.io7m.jlog.LogUsableType;
+import com.io7m.junreachable.UnreachableCodeException;
+
+final class VGLImplementation
+{
+  private VGLImplementation()
+  {
+    throw new UnreachableCodeException();
+  }
+
+  static JCGLImplementationType makeGLImplementation(
+    final GLContext ctx,
+    final LogUsableType in_log)
+  {
+    final JCGLImplementationJOGLBuilderType gb =
+      JCGLImplementationJOGL.newBuilder();
+    gb.setStateCaching(true);
+
+    final JCGLImplementationType gi = gb.build(ctx, in_log);
+    return gi;
+  }
+}
