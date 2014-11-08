fc9258e40332548d2826f409045a7bec1b232020
m0 <m0>
2014-11-08 12:16:23 +0000

    Move resource exception methods

--- io7m-r1-kernel/src/main/java/com/io7m/r1/kernel/KTextureUnitAllocator.java
+++ io7m-r1-kernel/src/main/java/com/io7m/r1/kernel/KTextureUnitAllocator.java
@@ -1,10 +1,10 @@
 /*
  * Copyright © 2014 <code@io7m.com> http://io7m.com
- *
+ * 
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
- *
+ * 
  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
  * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
@@ -30,6 +30,7 @@ import com.io7m.jequality.annotations.EqualityReference;
 import com.io7m.jnull.NullCheck;
 import com.io7m.jnull.Nullable;
 import com.io7m.r1.types.RException;
+import com.io7m.r1.types.RExceptionResource;
 import com.io7m.r1.types.RExceptionUnitAllocatorActive;
 import com.io7m.r1.types.RExceptionUnitAllocatorMultipleChildren;
 
@@ -253,7 +254,9 @@ import com.io7m.r1.types.RExceptionUnitAllocatorMultipleChildren;
     throws RException
   {
     if (KTextureUnitAllocator.this.hasEnoughUnits(index + 1) == false) {
-      throw RException.notEnoughTextureUnits(index + 1, this.units.size());
+      throw RExceptionResource.notEnoughTextureUnits(
+        index + 1,
+        this.units.size());
     }
 
     final TextureUnitType r = this.units.get(index);
