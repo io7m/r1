78041e7a5efe1ba877ad9ee5f3e988b22d6ca267
Mark Raynsford <code@io7m.com>
2014-11-11 15:49:44 +0000

    Finish documenting variance shadow mapping

--- io7m-r1-documentation/src/main/resources/com/io7m/r1/documentation/haskell/ProjectiveMatrix.hs
+++ io7m-r1-documentation/src/main/resources/com/io7m/r1/documentation/haskell/ProjectiveMatrix.hs
@@ -4,4 +4,7 @@ import qualified Matrix4f
 
 projective_matrix :: Matrix4f.T -> Matrix4f.T -> Matrix4f.T -> Matrix4f.T
 projective_matrix camera_view light_view light_projection =
-  Matrix4f.mult (Matrix4f.mult light_projection light_view) (Matrix4f.inverse camera_view)
+  case Matrix4f.inverse camera_view of
+    Just cv -> Matrix4f.mult (Matrix4f.mult light_projection light_view) cv
+    Nothing -> undefined -- A view matrix is always invertible
+
