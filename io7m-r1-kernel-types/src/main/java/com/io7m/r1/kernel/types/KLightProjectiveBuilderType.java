1a1c20a15145ce00346ac9f0b472625038166981
m0 <m0>
2014-11-13 22:26:54 +0000

    Improve projective light builders

--- io7m-r1-kernel-types/src/main/java/com/io7m/r1/kernel/types/KLightProjectiveBuilderType.java
+++ io7m-r1-kernel-types/src/main/java/com/io7m/r1/kernel/types/KLightProjectiveBuilderType.java
@@ -1,10 +1,10 @@
 /*
  * Copyright © 2014 <code@io7m.com> http://io7m.com
- *
+ * 
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
- *
+ * 
  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
  * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
@@ -28,6 +28,18 @@ import com.io7m.r1.types.RVectorI3F;
 
 public interface KLightProjectiveBuilderType
 {
+  /**
+   * <p>
+   * Set all values in the builder to those in the given existing light.
+   * </p>
+   * 
+   * @param k
+   *          The light
+   */
+
+  void copyFromProjective(
+    final KLightProjectiveType k);
+
   /**
    * <p>
    * Set the color of the light.
