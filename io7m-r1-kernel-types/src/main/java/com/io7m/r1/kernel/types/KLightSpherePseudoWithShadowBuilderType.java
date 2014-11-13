24f1da7e5695d089f3f0511ac7f67a8fffc5f613
Mark Raynsford <code@io7m.com>
2014-11-13 23:33:53 +0000

    Move function to common interface

--- io7m-r1-kernel-types/src/main/java/com/io7m/r1/kernel/types/KLightSpherePseudoWithShadowBuilderType.java
+++ io7m-r1-kernel-types/src/main/java/com/io7m/r1/kernel/types/KLightSpherePseudoWithShadowBuilderType.java
@@ -1,10 +1,10 @@
 /*
  * Copyright © 2014 <code@io7m.com> http://io7m.com
- * 
+ *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
  * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
@@ -22,6 +22,18 @@ package com.io7m.r1.kernel.types;
 
 public interface KLightSpherePseudoWithShadowBuilderType
 {
+  /**
+   * <p>
+   * Set all values in the builder to those in the given existing light.
+   * </p>
+   *
+   * @param s
+   *          The light
+   */
+
+  void copyFromPseudo(
+    final KLightSpherePseudoType s);
+
   /**
    * Enable/disable the negative X sub-light.
    *
@@ -126,7 +138,7 @@ public interface KLightSpherePseudoWithShadowBuilderType
    * <p>
    * Set whether or not the resulting lights will be diffuse-only.
    * </p>
-   * 
+   *
    * @param d
    *          <code>true</code> if the lights should be diffuse-only.
    */
